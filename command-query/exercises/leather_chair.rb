de2c7bc7991633e356df3a89fb4a697536f2c0c9
Eduardo Parra <helloeduardo@icloud.com>
2020-06-07 19:10:20 -0600

    Add objects for passing tests

---
+++ command-query/exercises/leather_chair.rb
@@ -0,0 +1,17 @@
+class LeatherChair
+  attr_reader :faded
+
+  def initialize
+    @faded = false
+  end
+
+  def faded?
+    faded
+  end
+
+  def expose_to_sunlight
+    @faded = true
+  end
+
+
+end
