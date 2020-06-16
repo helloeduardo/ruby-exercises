35a6ae31923c50f0f6816cbff9f5da03ae878b06
Eduardo Parra <helloeduardo@icloud.com>
2020-06-16 13:46:07 -0600

    Add basic enums tests

--- enumerables/exercises_1/basic_enums_test.rb
+++ enumerables/exercises_1/basic_enums_test.rb
@@ -6,18 +6,20 @@ class BasicEnumsTest < Minitest::Test
   def test_solve_for_x
     s = ''
     5.times do
-      # write code here
+      s += "x"
     end
     assert_equal 'xxxxx', s
   end
 
+  #number.sum enumerable
   def test_simple_sum
-    skip
+    # skip
     sum = 0
     numbers = [1, 2, 3, 4, 5]
-    # write code here
+    numbers.each do |number|
+      sum += number
+    end
     assert_equal 15, sum
   end
 
 end
-
